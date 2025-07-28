b527322309e7753127620644c9033f5259bcc678
victorhcunha <victor.cunha@liferay.com>
2025-07-28 12:49:57 -0300

    Mudar comando git para fetch --all

--- Chat.JS/start.cmd
+++ Chat.JS/start.cmd
@@ -1,4 +1,4 @@
 cd C:\xampp\htdocs
-git fetch upstream && git checkout upstream/main && git pull upstream main
+git fetch --all && git checkout upstream/main && git pull upstream main
 cd C:\xampp\htdocs\Chat.JS
 nodemon
